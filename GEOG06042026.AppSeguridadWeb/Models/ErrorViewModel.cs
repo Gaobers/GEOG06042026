3bc70b23d35a5cdec89ff84d5e43b0d964a593a1
Gabriel <gabrielobandogarcia100@gmail.com>
2026-04-06 10:34:43 -0600

    Agregar archivos de proyecto.

---
+++ GEOG06042026.AppSeguridadWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GEOG06042026.AppSeguridadWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
